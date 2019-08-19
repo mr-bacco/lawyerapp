562fb7411bb3b18cf05166030ceb9332a5707499
mr-bacco <mrbacco04@gmail.com>
2019-08-19 16:30:39 +0100

    first pages

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__) # creating an instalnce of the Flask class
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+@app.route("/")                             #defining the route for the index page
+def index():                                #creating the function to return the index page
+    return render_template("home.html")    #using a template and rendering the template
 
+
+# running this app in debug mode so that I can update the app.py without the need of manual restart
 if __name__ == "__main__":
     app.run(debug=True)
