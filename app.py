3a48c10ce8c7bed49f662d568b2e6440cf2df9aa
mr-bacco <mrbacco04@gmail.com>
2019-08-19 17:35:44 +0100

    added the articles page

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask, render_template
+from data import Articles
+
+Articles = Articles()
 
 app = Flask(__name__) # creating an instalnce of the Flask class
 
-@app.route("/")                             #defining the route for the index page
-def index():                                #creating the function to return the index page
-    return render_template("home.html")    #using a template and rendering the template
+####### defining the routes for the different web pages START #######
+@app.route("/")
+def index():
+    return render_template("home.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
+@app.route("/articles")
+def articles():
+    return render_template("articles.html", articles = Articles)
+
+####################################################################################################
 # running this app in debug mode so that I can update the app.py without the need of manual restart
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': '1',
+            'name': 'number 1',
+            'created': '10/5/19',
+            'type': 'something',
+            'author': 'bac'
+        },
+        {
+            'id': '2',
+            'name': 'number 2',
+            'created': '11/5/19',
+            'type': 'nothing',
+            'author': 'and'
+        },
+        {
+            'id': '3',
+            'name': 'number 3',
+            'created': '10/5/19',
+            'type': 'almost',
+            'author': 'and'
+        }
+    ]
+    return articles
