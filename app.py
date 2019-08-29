2dcdd49f3ecc8033e65a75b3f53d6d1b62782440
mr-bacco <mrbacco04@gmail.com>
2019-08-29 18:10:05 +0100

    register working now

--- app.py
+++ app.py
@@ -21,14 +21,19 @@ app = Flask(__name__) # creating an instalnce of the Flask class for thsi app as
 ############## db SETUP START ##############
 # using mongo db cloud version
 # checking the connection to cloud ongodb and printing in the console the list of collections under the database
-myclient = pymongo.MongoClient("mongodb://mrbacco:mongodb001@cluster0-shard-00-00-goutv.mongodb.net:27017,cluster0-shard-00-01-goutv.mongodb.net:27017,cluster0-shard-00-02-goutv.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
-mydb = myclient["database001"]
-print ("if connected to db, then these are the collections in mydb: ", mydb.list_collection_names()) #used to check if db is connected
+
+try:
+    myclient = pymongo.MongoClient("mongodb://mrbacco:mongodb001@cluster0-shard-00-00-goutv.mongodb.net:27017,cluster0-shard-00-01-goutv.mongodb.net:27017,cluster0-shard-00-02-goutv.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
+    mydb = myclient["database001"]
+    mycol = mydb["python001"]
+    print ("if connected to db, then these are the collections in mydb: ", mydb.list_collection_names()) #used to check if db is connected
+except:
+    print("Could not connect to MongoDB")
 
 ############## db SETUP END ##############
 
 
-############## Web scraping START ##############
+############## Web scraping example START ##############
 '''
 #making the soup
 with open("./templates/home.html") as file:
@@ -39,7 +44,7 @@ print(soup.prettify)
 
 '''
 
-############## Web scraping END ##############
+############## Web scraping example END ##############
 
 
 
@@ -61,25 +66,43 @@ def articles():
 def article(id):
     return render_template('list_item.html', id = id)
 
-@app.route('/register', methods = ['GET'])
-def register():
-    return render_template('register.html')
-'''
-
-class RegForm(Form):
+#definition of a class for the registration form
+class Reg(Form):
     name = StringField('Name', [validators.Length(min = 1, max = 50)])
     username = StringField('Username', [validators.Length(min = 5, max = 50)])
     email = StringField('Email', [validators.Length(min = 6, max = 50)])
-    password = PasswordField('Password')
+    password = PasswordField('Password', [validators.DataRequired()])
 
-@app.route('/register', methods = ['GET'])
+@app.route('/register', methods = ['GET', 'POST'])
 def register():
-    form = RegForm(request.form)
-    if request.method == 'POST' and form.validate():
-        return render_template('register.html', form = form), 404
+    form = Reg(request.form)
+    if request.method == 'GET': # make sure the method used is define above
+        return render_template('register.html', form = form), print("you are under the register page now using GET")
+    elif request.method == 'POST' and form.validate():
+
+        # the following are the values from the registration form
+        name = form.name.data
+        username = form.username.data
+        email = form.email.data
+        password = sha512_crypt.hash(str(form.password.data)) # passsword is hashed
+
+        myuser=[{ # defining a new variable taking as input the calues from the registration form
+                "name": name, 
+                "username": username, 
+                "email" : email, 
+                "password" : password
+                }]
+        x = mycol.insert_many(myuser), print("inserting this user named  ", myuser, "in the database called ", mycol)
+        
+        return render_template('register.html', form = form), print("you are under the register page now using POST, data are sent to server")
     return
-'''
+
+
+
+
+
 ############## defining the routes for the different web pages END ##############
+
 
 
 
--- data.py
+++ data.py
@@ -1,14 +1,14 @@
 def Items():
     items = [
         {
-            'id': '1',
+            'id': '100',
             'name': 'number 1',
             'created': '10/5/19',
             'type': 'something',
             'author': 'bac'
         },
         {
-            'id': '2',
+            'id': '276',
             'name': 'number 2',
             'created': '11/5/19',
             'type': 'nothing',
