9eebaf46830c4d04e640a380ecbc8be1faef0d0a
mr-bacco <mrbacco04@gmail.com>
2019-10-11 19:26:56 +0100

    functional single page made, CRUD enabled

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-############################################################
-# Project: web application for data science / web scraping #
-# Author: bac [and_bac]                                    #
-# Date: 2019                                               #
-############################################################
+########################################################
+# Project: basic web application for lawyers           #
+# Author: bac [and_bac]                                #
+# email: mrbacco@outlook.com                           #
+# Date: Oct 2019                                       #
+########################################################
 
-from flask import Flask, render_template, url_for, session, request, redirect, logging, flash  # modules from flask
+from flask import Flask, render_template, url_for, session, request, redirect, logging, flash  # modules from flask for web server
 from data import Items
 import pymongo # driver for mongdb connectivity
-import pandas as pd
-import scrapy as scrapy
-from bs4 import BeautifulSoup # used for web scraping
+# import pandas as pd
+# import scrapy as scrapy
+# from bs4 import BeautifulSoup # used for web scraping
 import requests # the request HTTP library allows managing HTTP request/responses to/from websites
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators #FOR THE WEBFORMS
 from passlib.hash import sha512_crypt #passowrd hashing
@@ -26,121 +27,48 @@ app = Flask(__name__) # creating an instalnce of the Flask class for thsi app as
 
 try:
     myclient = pymongo.MongoClient("mongodb://mrbacco:mongodb001@cluster0-shard-00-00-goutv.mongodb.net:27017,cluster0-shard-00-01-goutv.mongodb.net:27017,cluster0-shard-00-02-goutv.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
-    mydb = myclient["database001"]
-    mycol = mydb["python001"]
+    mydb = myclient["lawyers"]
+    mycol = mydb["feedback"]
     print("if connected to db, then these are the collections in mydb: ", mydb.list_collection_names()) #used to check if db is connected
 except:
     logging.warning("Could not connect to MongoDB")
 
 ############## db SETUP END ##############
 
-############## function to protect views if not logged in START ##############
-def isAuth(func):
-    @wraps(func)
-    def wrap (*args, **kwargs):
-        if "logged_in" in session:
-            return func(*args, **kwargs)
-        else: 
-            error = 'Need to login, first'
-            return redirect(url_for("login"), error), print ("user not registered, redirecting now to login page")
-    return wrap
-
-############## function to protect views if not logged in END ##############
-
-
 ############## defining the routes for the different web pages START ##############
-@app.route("/")
-def index():
-    return render_template("home.html")
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-@app.route("/articles")
-@isAuth
-def articles():
-    return render_template("articles.html", items = Items)
-
-@app.route('/list_item/<string:id>/')
-@isAuth
-def article(id):
-    return render_template('list_item.html', id = id)
-
-@app.route("/dashboard")
-@isAuth
-def dashboard():
-    return render_template("dashboard.html")
-
-class Reg(Form): #definition of a class for the registration form
+class Init(Form): #definition of a class for the init form
     name = StringField('Name', [validators.Length(min = 1, max = 50)])
-    username = StringField('Username', [validators.Length(min = 5, max = 50)])
+    telefono = StringField('telefono', [validators.Length(min = 5, max = 50)])
     email = StringField('Email', [validators.Length(min = 6, max = 50)])
-    password = PasswordField('Password', [validators.DataRequired()])
+    messaggio = StringField('Messaggio', [validators.DataRequired()])
 
-@app.route('/register', methods = ['GET', 'POST'])
-def register():
-    form = Reg(request.form)
+@app.route("/", methods = ['GET', 'POST'])
+def index():
+    form = Init(request.form)
     if request.method == 'GET': # make sure the method used is define above
-        return render_template('register.html', form = form), logging.warning("you are under the register page now using GET, well done bacco ")
+        return render_template('home.html', form = form), logging.warning("you are under the home page now using GET, well done bacco ")
     elif request.method == 'POST' and form.validate():
-        name = form.name.data                           # the following are the data from the registration form
-        username = form.username.data
+        name = form.name.data                           # the following are the data from the init form
+        telefono = form.telefono.data
         email = form.email.data
-        password = sha512_crypt.hash(str(form.password.data)) # passsword is encrypted
+        messaggio = form.messaggio.data
 
-        # defining a new variable taking as input the values from the registration form
-        myuser=[{ 
+        # defining a new variable taking as input the values from the init form
+        mymsg=[{ 
                 "name": name, 
-                "username": username, 
+                "telefono": telefono, 
                 "email" : email, 
-                "password" : password
+                "messaggio" : messaggio
                 }]
         # insert the list into the mongo db
-        x = mycol.insert_many(myuser), print("inserting this user: ", myuser, "in the database called ", mycol)
+        x = mycol.insert_many(mymsg), print("inserting this user: ", mymsg, "in the database called ", mycol)
         
-        return render_template('register.html', form = form), print("you are under the register page now using POST, data are sent to database")
+        return render_template('home.html', form = form), print("you are under the home page now using POST, data are sent to database")
     else: 
         print("this is the error")
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None    #declaration for this variable
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password_req = request.form.get('password')
-        newuser = username
-        if request.form.get('username') is not "" and request.form.get('password') is not "":
-            newuser = mycol.find_one({'username': username })
-            if newuser is not None:
-                password = newuser["password"] #return PasswordField for that username
-                print ("inserted username is: ", newuser )
-            else:
-                error = 'Invalid credentials again 2'
-                print ("user not present therefore moving on")
-                return render_template('login.html', error=error)
-            if sha512_crypt.verify(password_req, password):
-                print ("password matched", password_req, password)
-                #creating a session for the user just logged in
-                session["logged_in"] = True
-                session["username"]= username
-                return redirect(url_for("dashboard"))
-                print ("under the dashboard page now")
-            else:
-                error = 'Invalid credentials again 1'
-                print ("password DON'T match", password_req, password)
-                return render_template('login.html', error=error)         
-        else:
-            error = 'Empty credentials, try again please.'
-            print (error, "inserted username is: ", newuser )
-    return render_template('login.html', error=error)
 
-@app.route("/logout")
-@isAuth
-def logout():
-    session.clear()
-    return redirect(url_for("about"))
-    print("session destroyed for user ") #destroying session for logged in current user
+
 
 
 ############## defining the routes for the different web pages END ##############
