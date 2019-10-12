fa9d1e30a0e266e37bba0142487dc85bfa70df2b
mr-bacco <mrbacco04@gmail.com>
2019-10-12 17:30:49 +0100

    setup

--- setup.py
+++ setup.py
@@ -6,9 +6,5 @@ setup(name='lawerapp',
       author='BAC',
       author_email='mrbacco@outlook.com',
       url='https://www.python.org/sigs/distutils-sig/',
-      packages=['Flask==1.1.1', 'Flask-Login==0.4.1', 
-                'Flask-Mail==0.9.1', 'Jinja2==2.10.1', 'lxml==4.4.1', 
-                'passlib==1.7.1', 'pymongo==3.9.0', 
-                'urllib3==1.25.3',
-                'WTForms==2.2.1'])
+      packages=[])
 
